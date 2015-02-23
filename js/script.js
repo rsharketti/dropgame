88786ce84afa7f57695b59ed696ffd747f91824f
rsharketti <rsharketti@gmail.com>
2015-02-23 11:59:37 -0500

    Keyboard Controls,  Collisions.
    adding a bucket to catch the drops, and set up a function for the
    collision if the bucket catches the drop.

--- js/script.js
+++ js/script.js
@@ -7,10 +7,39 @@
  var spawntimer;
  var dropArray = new Array();
  var movetimer;
+ var user_bucket = new bucket(50,375,100,100);
+ 
+ /**
+  *this class is the user-controlled element that catches the drops
+   */
+ function bucket(ux, uy, uwidth, uheight){
+		this.x=ux;
+		this.y=uy;
+		this.width=uwidth;
+		this.height=uheight;
+		this.item_on_page;
+		
+		/** 
+		 *	The create method creates a DIV that looks like a red violet drop on the page
+		 */ 
+		 
+		 this.create = function(){
+			 this.item_on_page = document.createElement("div");
+			 this.item_on_page.style.left = this.x + 'px';
+			 this.item_on_page.style.top = this.y + 'px';
+			 this.item_on_page.style.width = this.width + 'px';
+			 this.item_on_page.style.height = this.height + 'px';
+			 this.item_on_page.style.position = 'absolute';
+			 this.item_on_page.style.backgroundColor = "rgba(20,0,125,.5)";
+			 document.body.appendChild(this.item_on_page);
+		 }//end create function
+ }//end bucket class
  
 function Drop(){
 	this.x; //starts empty, will keep track of each drop's left-right position as a #
 	this.y; //starts empty, will keep track of each drop's up-down position as a #
+	this.width=50;//keep track of drops width, for collisions.
+	this.height=50;
 	this.item_on_page; //represents drop's physical presence on the screen
 	/** 
 	*	The create method creates a DIV that looks like a blue drop on the page
@@ -66,8 +95,34 @@ function init() {
 	//when game starts, start causing a spawn function
 	//to happen every so ofter.
 	spawntimer = setInterval(spawn,1000);
+	//spawn();
 	movetimer = setInterval(moveAllDrops,1000/30);
+	//make a user controlled bucket object on the screen.
+	user_bucket.create();
+	//event handler:when a key is pressed, do the checkKey function
+	document.onkeydown = function(e){
+		checkKey(e);	
+	}
 }
+/**
+ *this method moves the bucket if appropriate key was hit.
+ */
+ 
+function checkKey(e){
+	e = e||window.event;//redefine the event references for all browsers
+	//if the key pressed was the right arrow
+	if(e.keyCode == 39){
+		//add to bucket's x
+		user_bucket.x += 20;
+	}else if(e.keyCode == 37){
+		//subtract from bucket's x
+		user_bucket.x += -20;
+	}
+	//apply its new x to its CSS position.
+	user_bucket.item_on_page.style.left = user_bucket.x + 'px';
+	
+}
+
 /**
  *generate a new drop objects every so ofter
  */
@@ -94,7 +149,38 @@ function moveAllDrops(){
 			if(currentdrop.y >400){
 					currentdrop.destroy();
 			}
+			//if this drop is colliding with the bucket...
+			if(collisionCheck(user_bucket, currentdrop)){
+			//get rid of this drop 
+				currentdrop.destroy();
+			//update counter and display
+			}
+			
 		}
 }
-
+/**
+ * this method returns true if the two elements are touching.
+ * @param	big_obj an HTML element 
+ * @param	sm_obj an HTML element 
+ * @return 	Boolean
+ */
+function collisionCheck(big_obj, sm_obj){
+	var bigL = big_obj.x;
+	var bigR = big_obj.x + big_obj.width;
+	var smL = sm_obj.x;
+	var smR = sm_obj.x + sm_obj.width;
+	var bigT = big_obj.y;
+	var bigB = big_obj.y + big_obj.height;
+	var smT = sm_obj.y;
+	var smB = sm_obj.y + sm_obj.height;
+		//if big_obj is touching sm_obj..
+		//Stricter: (smR < bigR) && (smL > bigL) 
+		if((smR > bigL )&&(smL < bigR)){
+			if((smT < bigB )&&(smB > bigT)){
+			//console.log(smR + " " + bigR + "and" + smL + " "+  bigL);
+			return true  
+			//otherwise return false
+			}//end height check
+		}//end width check
+}//end function collisionCheck
 
